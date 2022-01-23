2f580fae7117b746fb0188caf1d2b4ce7f043f13
pablo <pablo@pablos-MacBook-Pro.local>
2022-01-23 22:43:34 +0100

    Starting and stoping motors using the releay

--- Container.py
+++ Container.py
@@ -1,8 +1,10 @@
 from dependency_injector import providers, containers
 
 from controller.MotorController import MotorController
+from controller.RelayController import RelayController
 
 
 class Container(containers.DeclarativeContainer):
     config = providers.Configuration()
+    relay_controller = providers.Factory(RelayController, 4)
     motor_controller = providers.Singleton(MotorController)
--- domain/usecase/HandleCoverUseCase.py
+++ domain/usecase/HandleCoverUseCase.py
@@ -2,18 +2,32 @@ from dependency_injector.wiring import Provide, inject
 
 from Container import Container
 from controller.MotorController import MotorController
+from controller.RelayController import RelayController
+from controller.RelayStatus import RelayStatus
 
 
 class HandleCoverUseCase:
 
     @inject
-    def __init__(self, motor_controller: MotorController = Provide[Container.motor_controller]):
+    def __init__(self, motor_controller: MotorController = Provide[Container.motor_controller],
+                 relay_controller: RelayController = Provide[Container.relay_controller]):
         self.__motor_controller = motor_controller
+        self.__relay_controller = relay_controller
 
     def cover_up(self):
+        self.__start_motor()
         self.__motor_controller.set_direction_up()
         self.__motor_controller.move_motor()
+        self.__stop_motor()
 
     def cover_down(self):
+        self.__start_motor()
         self.__motor_controller.set_direction_down()
         self.__motor_controller.move_motor()
+        self.__stop_motor()
+
+    def __start_motor(self):
+        self.__relay_controller.update_relay_status(RelayStatus.ON)
+
+    def __stop_motor(self):
+        self.__relay_controller.update_relay_status(RelayStatus.OFF)
