1dc7eb57506b872d8509f6c75094102001b9284a
pabloazana <pablo.azana@gmail.com>
2022-03-05 18:43:30 +0100

    Some small refactors due to code review

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from domain.usecase.HandleCoverUseCase import HandleCoverUseCase
 from domain.usecase.HandleEmptyBombUseCase import HandleEmptyBombUseCase
 from domain.usecase.HandleFillingBombUseCase import HandleFillingBombUseCase
 
+COVER_UP_STEP = "COVER_UP"
+COVER_DOWN_STEP = "COVER_DOWN"
+FILLING_BOMB_ON_STEP = "FILLING_BOMB_ON"
+FILLING_BOMB_OFF_STEP = "FILLING_BOMB_OFF"
+EMPTY_BOMB_ON_STEP = "EMPTY_BOMB_ON"
+EMPTY_BOMB_OFF_STEP = "EMPTY_BOMB_OFF"
+
 if __name__ == '__main__':
 
     container = Container()
@@ -25,17 +32,17 @@ if __name__ == '__main__':
             while True:
                 message = client.recv(1024)
                 if message:
-                    if message.decode("UTF-8") == "COVER_UP":
+                    if message.decode("UTF-8") == COVER_UP_STEP:
                         handle_cover.cover_up()
-                    elif message.decode("UTF-8") == "COVER_DOWN":
+                    elif message.decode("UTF-8") == COVER_DOWN_STEP:
                         handle_cover.cover_down()
-                    elif message.decode("UTF-8") == "FILLING_BOMB_ON":
+                    elif message.decode("UTF-8") == FILLING_BOMB_ON_STEP:
                         handle_water_bomb.fill_aquarium()
-                    elif message.decode("UTF-8") == "FILLING_BOMB_OFF":
+                    elif message.decode("UTF-8") == FILLING_BOMB_OFF_STEP:
                         handle_water_bomb.stop_filling_aquarium()
-                    elif message.decode("UTF-8") == "EMPTY_BOMB_ON":
+                    elif message.decode("UTF-8") == EMPTY_BOMB_ON_STEP:
                         empty_aquarium_bomb.empty_aquarium()
-                    elif message.decode("UTF-8") == "EMPTY_BOMB_OFF":
+                    elif message.decode("UTF-8") == EMPTY_BOMB_OFF_STEP:
                         empty_aquarium_bomb.stop_empty_aquarium()
                 else:
                     break
